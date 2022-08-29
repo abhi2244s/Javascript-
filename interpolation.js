8c12c7f3e7b1c1a253efc0890d1de727d2b7b00b
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-08-29 21:58:40 +0530

    interpolation program

---
+++ interpolation.js
@@ -0,0 +1,11 @@
+let firstName = "Abhishek";
+let lastName  = "Singh";
+let maritalStatus = "Single"
+let country = "India"
+let age = 20;
+
+console.log(`My name is ${firstName} 
+LastName is ${lastName}
+marital status is ${maritalStatus}
+coutry is ${country}
+age is ${age}`)

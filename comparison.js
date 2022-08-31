8f96116981bd675185c40b3cb7a20e0d94123d04
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-09-01 01:01:41 +0530

    comparison program

---
+++ comparison.js
@@ -0,0 +1,22 @@
+// let str = "python"
+// let result = str.length
+
+// if (result==="6"){
+//     console.log("Is this true")
+// }
+
+// comparison
+//   4 > 3 = true
+// - 4 >= 3 = t
+// - 4 < 3 =f
+// - 4 <= 3= f
+// - 4 == 4 =t
+// - 4 === 4 =t
+// - 4 != 4 =f
+// - 4 !== 4 =f
+// - 4 != '4'f
+// - 4 == '4' =t
+// - 4 === '4'=f
+
+let number = 4 === '4' ;
+console.log(number)

2c3f67d0eb79acb0d58cc001c9f3a1e1e2032c46
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-08-31 15:00:50 +0530

    lastindexof  praogram

---
+++ lastindexof.js
@@ -0,0 +1,8 @@
+let str =  "Hello , Abhishek ,Hope you are doing great";
+let result = str.lastIndexOf('great')
+console.log(result)
+
+// let arr = ['Apple', 'Mango', 'Banana'];
+
+// let result = arr.lastIndexOf('Apple')
+// console.log(result)

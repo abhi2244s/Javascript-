45dae83a5c7a0191aa653fa87a010b5b4e449d43
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-09-01 01:02:32 +0530

    Date program

---
+++ date.js
@@ -0,0 +1,14 @@
+let date = new Date()
+console.log(date.getFullYear())
+
+console.log(date.getMonth())
+
+
+console.log(date.getDate())
+
+
+console.log(date.getHours())
+
+
+console.log(date.getMinutes())
+
---
+++ date1.js
@@ -0,0 +1,4 @@
+// Find out the numbers of seconds elapsed from January 1, 1970 to now.
+let date =  new Date()
+let seconds = date.getSeconds()
+console.log(seconds)

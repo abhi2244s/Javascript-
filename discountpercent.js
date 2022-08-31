87fa2ea1f344cafb35e3eae9b813c82cdb75018f
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-09-01 01:05:41 +0530

    discount program

---
+++ discountpercent.js
@@ -0,0 +1,9 @@
+let sellingPrice = 799;
+let listingPrice = 1999;
+
+let discountPercent = ((listingPrice-sellingPrice)/listingPrice *100)
+
+console.log("Discount Percentage is : "+ discountPercent)
+
+displayPercentage = Math.round(discountPercent)
+console.log(discountPercent)

2e31fc78911e2b2899963a427e89a169c73295ce
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-09-03 23:25:59 +0530

    closure.js

---
+++ closure.js
@@ -0,0 +1,13 @@
+function greeting() {
+    let message = 'Hi';
+
+    function sayHi() {
+
+        let name = "Abhishek Singh"
+        console.log(message + " " + name);
+    }
+
+    sayHi();
+}
+
+greeting()

017b3e08b18a75a1a43aebe550daa8f4dee26004
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-09-09 00:11:21 +0530

    month days finding program

---
+++ monthdate.js
@@ -0,0 +1,13 @@
+// Month in JavaScript is 0-indexed (January is 0, February is 1, etc), 
+// but by using 0 as the day it will give us the last day of the prior
+// month. So passing in 1 as the month number will return the last day
+// of January, not February
+function daysInMonth (month, year) {
+    return new Date(year, month,0 ).getDate();
+}
+
+// July
+console.log(daysInMonth(2,20022)); // 31
+// February
+daysInMonth(2,2009); // 28
+daysInMonth(2,2008); // 29

68d9303d634b49bb5490cb323631438829ee6a1d
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-09-10 16:37:24 +0530

    array methods

---
+++ revise/array/arr_slice.js
@@ -0,0 +1,4 @@
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(3);
+// slice it creates a new array
+console.log(citrus)
---
+++ revise/array/arr_splice.js
@@ -0,0 +1,8 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+// ist is telling about position where new element should be added
+
+// 2nd is telling about how many elemnets should be removed
+
+console.log(fruits)

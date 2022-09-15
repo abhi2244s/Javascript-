a13aff5dab9875a0c3bc59486741d4af10fc9e5e
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-09-15 20:00:31 +0530

    map program

---
+++ revise/array/array_iteration/map.js
@@ -0,0 +1,20 @@
+const number = [5, 2, 3, 4];
+
+// function double(x){
+//     return x*2
+// }
+
+
+const output = number.map(binary)
+
+
+function binary(x){
+    return x.toString(2)
+}
+
+console.log(output)
+
+
+
+
+
